0c4650aa9625715799ea3c1254cdbb96a6d46e9c
kethrin7 <ketevaniburjanadze@gmail.com>
2023-04-24 13:13:45 +0400

    add new project

---
+++ src/components/screens/Home.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/constants/Routes.js
@@ -0,0 +1,13 @@
+// AUTH NAVIGATION SCREENS
+export const SIGN_IN_SCREEN = "SignIn";
+export const FORGOT_PASSWORD_SCREEN = "ForgotPassword";
+export const SIGN_UP_SCREEN = "SignUp";
+
+// HOME NAVIGATION SCREENS
+export const BOTTOM_TAB_NAV = "bottomTabNavigator";
+export const SETTINGS_NAVIGATOR = "SettingsNavigator";
+export const HOME_SCREEN = "HomeScreen";
+export const ORDERS_SCREEN = "OrderScreen";
+export const SHOP_SCREEN = "ShopScreen";
+export const SETTINGS_SCREEN = "SettingsScreen";
+export const SETTINGS_DETAILS = "SettingsDetails";
